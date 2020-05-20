be0cd77179696c062939f5e2cb4ef93955050d5a
紫雨晴 <61619265+qyz-admin@users.noreply.github.com>
2020-05-20 15:40:12 +0800

    Update admin-2.0.js

--- admin-2.0.js
+++ admin-2.0.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
-// @name         admin-5.23
+// @name         admin-5.24
 // @namespace    http://tampermonkey.net/
-// @version      5.23
+// @version      5.24
 // @description  以客服查詢界面為主
 // @author       qyz
 // @match        http://13.229.176.203/admin/index/index.html
