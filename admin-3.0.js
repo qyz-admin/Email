f7f2026b14d49073754bb61da42a3a4ed9bb96e4
紫雨晴 <61619265+qyz-admin@users.noreply.github.com>
2020-06-19 11:51:53 +0800

    Update admin-3.0.js

--- admin-3.0.js
+++ admin-3.0.js
@@ -518,7 +518,7 @@ var vm = new Vue({
                               // data = data + dataneme +";-----电话：" + iptxt +";-----           " + "\n" + "\n";
                                data = data + dataneme +","
                            }else{
-                               layer.msg("没有重复订单哦", {icon: 6},{time: 500});
+                               layer.msg("重复订单哦", {icon: 6},{time: 500});
                            }
                       }
                }
@@ -553,7 +553,7 @@ var vm = new Vue({
                               // data = data + dataneme +";-----拉黑率：" + iptxtval +";-----电话："+ iptxt +";-----           " + "\n" + "\n";
                                 data = data + dataneme +","
                            }else{
-                              layer.msg("没有拉黑订单哦", {icon: 6},{time: 500});
+                              layer.msg("拉黑订单哦", {icon: 6},{time: 500});
                            }
                       }
                }
@@ -587,7 +587,7 @@ var vm = new Vue({
                              //  data = data + dataneme +";-----电话：" + iptxtval +";-----IP："+ iptxt +";-----           " + "\n" + "\n";
                                 data = data + dataneme +","
                            }else{
-                              layer.msg("没有ip重复订单哦", {icon: 6},{time: 500});
+                              layer.msg("ip重复订单哦", {icon: 6},{time: 500});
                            }
                       }
                }
