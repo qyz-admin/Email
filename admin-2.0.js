4e91fdd2b2df6f4139ed8d8eb9b5d4bed23b375f
紫雨晴 <61619265+qyz-admin@users.noreply.github.com>
2020-07-19 19:46:10 +0800

    Update admin-2.0.js

--- admin-2.0.js
+++ admin-2.0.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
-// @name         admin-5.35
+// @name         admin-5.36
 // @namespace    http://tampermonkey.net/
-// @version      5.35
+// @version      5.36
 // @description  以客服查詢界面為主
 // @author       qyz
 // @match        http://13.229.176.203/admin/index/index.html
@@ -1448,9 +1448,10 @@ document.getElementById("ffz_searchButton").addEventListener("click", function f
    function getImg(){
 	    if(currentIndex >= item_arr.length){
            wxcx.value = "";
-           show_nav();
+          // show_nav();
+		            dcountSum();//计数
            layer.msg("已全部查詢完了", {icon: 6},{time: 500});
-           dcountSum();//计数
+  
             // cx.click();
 		  return;
         };
@@ -1626,9 +1627,9 @@ document.getElementById("ffz_searchButton").addEventListener("click", function f
    function getImg(){
 	    if(currentIndex >= item_arr.length){
            wxcx.value = "";
-           show_nav();
+          // show_nav();
+		                countSum();
            layer.msg("已全部查詢完了", {icon: 6},{time: 500});
-            countSum();
             // cx.click();
 		  return;
         };
