57ca10b4e6c1c315052d721ff75d639bc291acb9
紫雨晴 <61619265+qyz-admin@users.noreply.github.com>
2020-08-26 14:04:00 +0800

    Update demoBPS-1.0.js

--- demoBPS-1.0.js
+++ demoBPS-1.0.js
@@ -3,7 +3,7 @@ function daochu1(){
         var exportFileContent = document.getElementsByClassName("table table-striped table-bordered table-hover")[0].outerHTML;
 	var template = '<html xmlns:o="urn:schemas-microsoft-com:office:office"xmlns:x="urn:schemas-microsoft-com:office:excel"xmlns="http://www.w3.org/TR/REC-html40">'+
         '<head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet>'+
-        '<x:Name>'+worksheet+'</x:Name>'+
+        '<x:Name>worksheet</x:Name>'+
         '<x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet>'+
         '</x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]-->'+
         '</head><body><table>'+exportFileContent+'</table></body></html>';
