9634ba290ac0c3a513ae5dfa65c592563096fcb1
紫雨晴 <61619265+qyz-admin@users.noreply.github.com>
2020-08-29 13:51:27 +0800

    Update admin-1.0.js

--- admin-1.0.js
+++ admin-1.0.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
-// @name         admin-5.25--
+// @name         admin-5.26--
 // @namespace    http://tampermonkey.net/
-// @version      5.25
+// @version      5.26
 // @description  以客服查詢界面為主
 // @author       qyz
 // @match        http://13.229.176.203/admin/index/index.html
@@ -2725,7 +2725,7 @@ var thNodeVal = document.getElementById("tab_iframe_登記退換貨表").content
                     downloadA.href = window.URL.createObjectURL(blob);
                     downloadA.download = "数据下载.xlsx";
                     downloadA.click();
-               };
+           
 
    /*       var wxcx = document.getElementById("tab_iframe_客服查询").contentDocument.getElementsByClassName("form-control input-inline input-medium")[0];
           var wxcx_value=wxcx.value;
